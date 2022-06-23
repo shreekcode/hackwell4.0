332dc668db61b0e3ace02cdd82c98ca0fabceb1b
RishiBelani <rishibelani@gmail.com>
2022-06-23 17:41:08 +0530

    modified ui

--- Hackwell/main.php
+++ Hackwell/main.php
@@ -1,31 +1,36 @@
 <?php
-include 'Includes/header.php';
+    include 'Includes/header.php';
 ?>
 <style>
-  a {
-    color: inherit;
-  }
+    a {
+        color: inherit;
+    }
+    
+    textarea {
+        horizontal-align: left ;
+        resize:none;
+        display: inline-block;
+    }
 </style>
 
 <div class="tab">
   <button class="tablinks" onclick="openCity(event, 'LEARNING')">LEARNING</button>
-  <button class="tablinks"><a href="detection.php">DETECTION</a></button>
+  <button class="tablinks" ><a href = "detection.php">DETECTION</a></button>
 </div>
 
 <!-- Tab content -->
 <div id="LEARNING" class="tabcontent">
   <h3>LEARNING</h3>
   <p>ADD A TEXT FILE!!</p>
-  <form action="upload_file.php" method="post" enctype="multipart/form-data">
-    <label for="file">Filename:</label>
-    <input type="file" name="file" id="file" />
-    <br />
-    <input type="submit" name="submit" value="Submit" />
-  </form>
+  <div> 
+    <input type="file">
+    <textarea cols="50" rows="20" placeholder="text will appear here">
+    </textarea>
+  </div>
+  <script src="display.js"></script>
 </div>
 
 </div>
-
 <?php
-include 'Includes/footer.php';
+    include 'Includes/footer.php';
 ?>
